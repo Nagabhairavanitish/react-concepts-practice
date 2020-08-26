611d4d54ca78ba587976120ddad7cd7b91624d65
nitish nagabhairava <nitishrockson@gmail.com>
2020-08-26 12:44:41 +0530

    added comments to the functionalitys

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  /*This is a stateful component. when we use state than it is stateful, 
+  these components usage must be reduced to make a better understanding of the code */
+
   state ={
     persons:[
       {name: "madara",age :40 },
@@ -11,6 +14,8 @@ class App extends Component {
     ],
     otherState: "not used"
   }
+  /*switch name handler is how we modify a name when we press the switch.
+  It is a one way binding where we just change the name by pressing switch. */ 
 
   switchNameHandler = (newName) => {
     //console.log("was clicked !")
@@ -37,20 +42,23 @@ class App extends Component {
   }
 
   render() {
+    /*sample example of how we perform the inline styling. 
+    Inline styling is basically adding css in the javascript file instead of creating a new app.css file*/
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
     }
+    /*In here we use person component that is imported from the persons folder.
+    switchHandler is how we use a oneway binding. nameChangeHandler is a classic example of two way binding*/
     return (
       <div className="App">
          <h1> hi my name is nitish</h1> 
          <p>working on React</p>
          <button 
             onClick= {this.switchNameHandler.bind(this, 'itachi')}
-            style={style}>Switch Name</button>
-        
+            style={style}>Switch Name</button>            
          <Person  
             name = {this.state.persons[0].name}
             age ={this.state.persons[0].age} 
